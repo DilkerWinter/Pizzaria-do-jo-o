7297253d2c21e86f85b565b05e311c29b26c7b16
Bruno Winter <brunohdwinter@gmail.com>
2024-02-23 15:48:26 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    include_once("templates/header.php");
+?>    
+
+
+    <h1>Olá mundo</h1>
+    <i class="fas fa-sync-alt"></i>
+<?php
+    include_once("templates/footer.php");
+?>
---
+++ templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça seu pedido!</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <a href="index.php" class="navbar-brand">
+                <img src="img/pizza.svg" alt="Pizzaria do João" id="brand-logo">
+            </a>
+            <div class="colapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a href="index.php" class="nav-link">Peça sua pizza</a>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+    </header>
